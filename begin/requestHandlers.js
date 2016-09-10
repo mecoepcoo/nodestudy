4308270b51a0821f89d2777cc56ea9a32c45fcda
tianzhen <340349237@qq.com>
2016-09-10 11:33:59 +0800

    test change

--- begin/requestHandlers.js
+++ begin/requestHandlers.js
@@ -1,9 +1,9 @@
 function start() {
-    console.log("Request handler 'start' was called!" );
+    console.log("Request handler 'start' was called!!!" );
     return "Hello start!";
 }
 function upload() {
-    console.log("Request handler 'upload' was called!")
+    console.log("Request handler 'upload' was called!!!")
     return "Hello upload!";
 }
 
