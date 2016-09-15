85252c0ebd3fbfed207f0d1cdcffbd879b5a5873
tianzhen <340349237@qq.com>
2016-09-15 16:03:17 +0800

    1

--- begin/server.js
+++ begin/server.js
@@ -2,14 +2,21 @@ var http = require("http");
 var url = require("url");
 function start(route, handle) {
     function onRequest(request, response){
+        var postData = "";
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
 
+        request.setEncoding("utf8");
 
-        response.writeHead(200,{"Content-Type":"text/plain"});
-        var content = route(handle, pathname);
-        response.write(content);
-        response.end();
+        request.addListener("data",function(postDataChunk){
+            postData += postDataChunk;
+            console.log("Received POST data chunk '"+
+                postDataChunk +"'.");
+        });
+
+        request.addListener("end",function(){
+            route(handle, pathname, response, postData);
+        });
     }
     http.createServer(onRequest).listen(8888);
     console.log("server start at http://127.0.0.1:8888")
